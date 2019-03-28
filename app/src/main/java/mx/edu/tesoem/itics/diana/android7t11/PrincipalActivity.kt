a6b7dbca1e6ed43790bc89f9bbb16a5ee509670f
Tesoem <Tesoem@ISC-Tesoem-iMac.local>
2019-03-28 08:31:33 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/itics/diana/android7t11/PrincipalActivity.kt
@@ -0,0 +1,12 @@
+package mx.edu.tesoem.itics.diana.android7t11
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PrincipalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}
